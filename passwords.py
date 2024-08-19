ec6e5b7103891603cd4900052553d862de6531b9
VeraKalinkin <153802442+VeraKalinkin@users.noreply.github.com>
2024-08-19 21:38:48 +0400

    Update passwords.py

--- passwords.py
+++ passwords.py
@@ -19,25 +19,30 @@ def password(flag, count, answer):
 
 
 def create_flag(flag):
-    print('Использовать цифры? да/нет')
-    if input() == 'нет':
+    print("Use numbers? y/n")
+    if input() == 'n':
         flag[0] = 1
-    print('Использовать строчные буквы? да/нет')
-    if input() == 'нет':
+    print("Use lowercase letters? y/n")
+    if input() == 'n':
         flag[1] = 1
-    print('Использовать заглавные буквы? да/нет')
-    if input() == 'нет':
+    print("Use uppercase letters? y/n")
+    if input() == 'n':
         flag[2] = 1
-    print('Использовать символы? да/нет')
-    if input() == 'нет':
+    print("Use punctuation? y/n")
+    if input() == 'n':
         flag[3] = 1
     return flag
 
-print('Введите длину пароля:')
-count = int(input())
-flag = [0, 0, 0, 0]
-answer = []
-flag = create_flag(flag)
-answer = password(flag, count, answer)
-random.shuffle(answer)
-print(*answer)
+
+def main():
+    print("What should be the length of your password?")
+    count = int(input())
+    flag = [0, 0, 0, 0]
+    answer = []
+    flag = create_flag(flag)
+    answer = password(flag, count, answer)
+    random.shuffle(answer)
+    print(*answer)
+
+
+main()
